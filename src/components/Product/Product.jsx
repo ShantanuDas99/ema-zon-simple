595fc39fae0414271b8976a88f525e07765d8668
ShantanuDas99 <procodeshanti@gmail.com>
2023-03-28 08:55:52 +0600

    favicon added and order added

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,8 +1,12 @@
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 import './Product.css';
 
 const Product = (props) => {
     const{name, img, price, ratings, seller} = props.product;
+    const handleAddToCart = props.handleAddToCart;
+
     return (
         <div className='product'>
             <img src={img} alt="" />
@@ -12,7 +16,10 @@ const Product = (props) => {
                 <p>Manufacturer: {seller}</p>
                 <p>Rating: {ratings}</p>    
             </div>
-            <button className='add-cart'>Add to cart</button>        
+            <button className='add-cart' onClick={()=> handleAddToCart(props.product)}>
+                Add to cart
+                <FontAwesomeIcon icon={faShoppingCart} />
+                </button>        
         </div>
     );
 };
